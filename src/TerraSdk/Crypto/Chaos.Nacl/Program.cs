0c5fa60acddf543260071ffe8ec95b8b63aa5a53
Daniel Grimes <dan@dcgnet.co.uk>
2021-10-01 20:16:33 +0100

    Add crypto libs

---
+++ src/TerraSdk/Crypto/Chaos.Nacl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TerraSdk.Crypto.Chaos.Nacl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/TerraSdk/Crypto/Hash.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using TerraSdk.Crypto.Chaos.Nacl;
+using TerraSdk.Util.Common;
+
+namespace TerraSdk.Crypto
+{
+    public static class Hash
+    {
+
+        static readonly Lazy<Ripemd160.Ripemd160> Ripemd160Lazy = new(() => new Ripemd160.Ripemd160());
+
+        public const int KeyLength = 32;
+        public const int NonceLength = 24;
+        public const int PublicKeyLength = 32;
+        public const int SeedLength = 32;
+        public const int SignatureLength = 64;
+
+        //public static SignatureAlgorithm Algorithm { get; } = SignatureAlgorithm.Ed25519;
+
+        public static byte[] EncryptSymmetric(byte[] message, byte[] nonce, byte[] sharedKey)
+        {
+            return XSalsa20Poly1305.Encrypt(message, sharedKey, nonce);
+        }
+        
+        public static byte[] DecryptSymmetric(byte[] message, byte[] nonce, byte[] sharedKey)
+        {
+            return XSalsa20Poly1305.TryDecrypt(message, sharedKey, nonce);
+        }
+
+        public static byte[] Sha256(byte[] input)
+        {
+            using var sha256 = SHA256.Create();
+            var hash = sha256.ComputeHash(input);
+            return hash;
+        }
+
+        public static byte[] Sha256(string input)
+        {
+            var inputBytes = Encoding.Default.GetBytes(input);
+
+            return Hash.Sha256(inputBytes);
+        }
+
+        //public static string Sha256Hex(string inputHex)
+        //{
+        //    var inputBytes = inputHex.FromHexString();
+
+        //    return Hash.Sha256(inputBytes).ToHexString();
+        //}
+
+        public static string DoubleSha256(byte[] input)
+        {
+            using (var sha256 = SHA256Managed.Create())
+            {
+                var hash = sha256.ComputeHash(input);
+
+                var doubleHash = sha256.ComputeHash(hash);
+
+                return doubleHash.ToHex();
+            }
+        }
+
+        public static string DoubleSha256(string input)
+        {
+            var inputBytes = Encoding.Default.GetBytes(input);
+
+            return Hash.DoubleSha256(inputBytes);
+        }
+
+        public static byte[] Ripemd160(string input)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(input);
+            var result = Ripemd160Lazy.Value.ComputeHash(bytes);
+            return result;
+        }
+
+        public static byte[] Ripemd160Hex(string inputHex)
+        {
+            var bytes = inputHex.ParseHex();
+            var result = Ripemd160Lazy.Value.ComputeHash(bytes);
+            return result;
+        }
+    }
+}
